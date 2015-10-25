a6c77f9a026009f5e63fa5e48c25e1bc2e832585
sexyandikit <jfarrell11@hotmail.com>
2015-10-24 21:25:43 -0400

    another edit

--- names.js
+++ names.js
@@ -109,7 +109,7 @@ function cfun(){
       document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/iEKADSh.png')";
       unamestuff.style.color='#D65CFF';
     }
-    if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='Ninjasebfan'){
+    if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='NinjasebFan'){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
       $(unamestuff).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
