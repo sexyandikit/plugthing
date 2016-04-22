cc535f7ad4dd100896be1e0bf4acb6a1d9bd95a5
sexyandikit <jfarrell11@hotmail.com>
2016-04-21 22:43:43 -0400

    brand new

--- names.js
+++ names.js
@@ -1,18 +1,11 @@
 var i=0;
 function cfun(){
     i++;
-    if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='Raiko-Senpai'){
+    if(document.getElementById('chat').lastChild.getElementsByClassName('username')[0].innerHTML==='raikosenpai'){
       i++;
-      unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
-      $(unamestuff).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/iEKADSh.png')";
+      unamestuff = document.getElementById('chat').lastChild.getElementsByClassName('username')[0];
+      $(unamestuff).before('<i id='+("icon-BFG"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-BFG'+i).style.backgroundImage = "url('http://i.imgur.com/iEKADSh.png')";
       unamestuff.style.color='#D65CFF';
-    }
-    if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='NinjasebFan'){
-      i++;
-      unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
-      $(unamestuff).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/Wyh8Mbv.png')";
-      unamestuff.style.color='#FF0000';
     }
 }
